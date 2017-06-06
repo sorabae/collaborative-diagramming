007da599dd8ff868989b7bb807249cef266e9c73
Sora Bae <qlthfk@gmail.com>
2017-06-07 01:24:29 +0900

    Until keyboard

---
+++ CollaborativeDiagramming/Assets/Scripts/EdgeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private GameObject fromObject;
+    private GameObject toObject;
+    private LineRenderer lr;
+    void OnConnectedToComponents(GameObject[] connection)
+    {
+        Debug.Log("OnConnectedToComponenets");
+        fromObject = connection[0];
+        toObject = connection[1];
+        lr = gameObject.AddComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        if (lr != null)
+        {
+            Debug.Log("LateUpdate");
+            lr.startWidth = 0.03f;
+            lr.endWidth = 0.03f;
+
+            lr.positionCount = 2;
+          
+            lr.SetPosition(0, fromObject.transform.position);
+            lr.SetPosition(1, toObject.transform.position);
+        }
+    }
+}
---
+++ CollaborativeDiagramming/Assets/Scripts/Interactible.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The Interactible class flags a Game Object as being "Interactible".
+/// Determines what happens when an Interactible is being gazed at.
+/// </summary>
+public class Interactible : MonoBehaviour
+{
+    private Material[] defaultMaterials;
+
+    void Start()
+    {
+        // Add a BoxCollider if the interactible does not contain one.
+        Collider collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    void GazeEntered()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Sphere")
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void GazeExited()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Sphere")
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
